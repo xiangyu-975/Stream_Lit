0ba0ed66ac36f16da1604cf6391516db689c7cc4
nick <xiangyu_975@163.com>
2023-05-22 12:43:20 +0800

    this is StreamLit

---
+++ myfile.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2023/5/15 17:54
+@Author  : Nick
+@File    : myfile.py.py
+@Software: PyCharm
+"""
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c']
+)
+print(chart_data)
+
+st.line_chart(chart_data)
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+print(df)
+st.line_chart(df)
